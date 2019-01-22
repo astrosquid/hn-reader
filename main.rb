c347c73e3196ba08b24a5eab5b862e6fb2d7e0e8
Chris Bojemski <chris.bojemski@gmail.com>
2019-01-22 14:23:01 -0500

    OOP because I am good at the programming

--- main.rb
+++ main.rb
@@ -1,23 +1,64 @@
 require 'nokogiri'
 require 'rest-client'
 
-url = 'https://news.ycombinator.com'
-home = RestClient.get(url)
-page = Nokogiri::HTML(home)
-links = page.css('a.storylink')
+class NewsReader
+  def initialize(opts, website)
+    @opts = opts
+    @website = website
+  end
 
-titles = links.map do |link|
-  if link.content.length > 49
-    link.content[0, 46] + '...'
-  else 
-    link.content
+  def read
+    headline_data = {} # int => [title, link]
+
+    @website.get
+    
+    @website.links.each_with_index do |link_tag, i|
+      index = i + 1
+      headline_data[index] = [link_tag.content, link_tag['href']]
+    end
+
+    self.print_headlines headline_data
+  end
+
+  def print_headlines(headline_data)
+    headline_data.each do |key, headline|
+      puts "#{key}: #{headline[0]}"
+    end
   end
 end
 
-titles.each_with_index do |title, i|
-  if i < 9
-    puts " #{i + 1}: #{title}"
-  else
-    puts "#{i + 1}: #{title}"
+class Options
+  attr_reader :opts
+
+  def initialize
+    args = Hash[ ARGV.flat_map{|s| s.scan(/--?([^=\s]+)(?:=(\S+))?/) } ]
+    opts = {:empty => []}
+    args.keys.each do |arg|
+      if !!args[arg]
+        opts[arg] = args[arg]
+      else
+        opts[:empty] << arg
+      end
+    end
+    @opts = opts
   end
 end
+
+class Website
+  attr_accessor :url, :home, :page, :links
+  
+  def initialize(url)
+    @url = url
+  end
+
+  def get
+    @home = RestClient.get(@url)
+    @page = Nokogiri::HTML(@home)
+    @links = @page.css('a.storylink')
+  end
+end
+
+opts = Options.new
+site = Website.new('https://news.ycombinator.com')
+reader = NewsReader.new(opts, site)
+reader.read
