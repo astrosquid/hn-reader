8b43422f439dcb5ec6e20879855216d8e8a72e6d
Chris Bojemski <chris.bojemski@gmail.com>
2019-01-23 01:19:56 -0500

    Move NewsReader to its own file, also ignore swap files.

--- main.rb
+++ main.rb
@@ -1,31 +1,4 @@
-require 'nokogiri'
-require 'rest-client'
-
-class NewsReader
-  def initialize(opts, website)
-    @opts = opts
-    @website = website
-  end
-
-  def read
-    headline_data = {} # int => [title, link]
-
-    @website.get
-    
-    @website.links.each_with_index do |link_tag, i|
-      index = i + 1
-      headline_data[index] = [link_tag.content, link_tag['href']]
-    end
-
-    self.print_headlines headline_data
-  end
-
-  def print_headlines(headline_data)
-    headline_data.each do |key, headline|
-      puts "#{key}: #{headline[0]}"
-    end
-  end
-end
+require_relative './NewsReader.rb'
 
 class Options
   attr_reader :opts
