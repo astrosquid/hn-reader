a8394cd619f345ef22ea211dd112dc135e2ba0d1
Chris Bojemski <chris.bojemski@gmail.com>
2019-01-25 00:55:09 -0500

    Test fetch and get_link_tags

--- lib/website.rb
+++ lib/website.rb
@@ -6,7 +6,14 @@ class Website
 
   @@site_ids = {
     1 => 'https://news.ycombinator.com',
-    2 => 'https://belong.io'
+    2 => 'https://belong.io',
+    3 => 'https://pinboard.in/popular'
+  }
+
+  @@link_selectors = {
+    1 => 'a.storylink',
+    2 => 'div.item > a',
+    3 => 'a.bookmark_title'
   }
 
   def initialize(site_id: 1)
@@ -15,6 +22,18 @@ class Website
 
   def site_url
     @@site_ids[@site_id]
+  end
+
+  def link_selector
+    @@link_selectors[@site_id]
+  end
+
+  def get_link_tags
+    @links = @html.css(self.link_selector)
+    if @site_id == 2
+      @links.pop
+    end
+    return @links
   end
 
   def fetch
--- spec/website_spec.rb
+++ spec/website_spec.rb
@@ -9,11 +9,65 @@ RSpec.describe Website, "#fetch" do
     end
   end
 
+  context "when initialized with 1" do 
+    it "fetches the Hacker News HTML" do 
+      website = Website.new(site_id: 1)
+      website.fetch
+      expect(website.html.to_s.include? "<title>Hacker News</title>").to eq true
+    end
+  end
+
   context "when initialized with 2" do
     it "fetches the BelongIO HTML" do 
       website = Website.new(site_id: 2)
       website.fetch
       expect(website.html.to_s.include? "<title>BELONG</title>").to eq true
+    end
+  end
+
+  context "when initialized with 3" do 
+    it "fetches the HTML from Pinboard's /popular page" do 
+      website = Website.new(site_id: 3)
+      website.fetch
+      expect(website.html.to_s.include? "<title>Pinboard: popular bookmarks</title>").to eq true
+    end
+  end
+end
+
+RSpec.describe Website, "#get_link_tags" do 
+  context "when initialized without args" do
+    it "uses the Hacker News selectors to find links" do 
+      hn = Website.new
+      hn.fetch
+      links = hn.get_link_tags
+      expect(links.length).to eq 30
+    end
+  end
+
+  context "when initialized with 1" do
+    it "uses the Hacker News selectors to find links" do 
+      hn = Website.new(site_id: 1)
+      hn.fetch
+      links = hn.get_link_tags
+      expect(links.length).to eq 30
+    end
+  end
+
+  context "when initialized with 2" do
+    it "uses the BelongIO css selectors to find links" do 
+      belong = Website.new(site_id: 2)
+      belong.fetch
+      links = belong.get_link_tags
+      expect(links.length).to eq 38
+    end
+  end
+
+  context "when initialized with 3" do 
+    it "uses the Pinboard css selectors to find links from popular bookmarks" do
+      pinboard = Website.new(site_id: 3)
+      pinboard.fetch
+      links = pinboard.get_link_tags
+      expect(links.length).to eq 100
     end
   end
 end
