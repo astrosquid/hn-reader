6aecf703610ddfe9413a3b5a13fcd9c9cc30844d
Chris Bojemski <chris.bojemski@gmail.com>
2019-01-30 18:57:15 -0500

    Before creating gem

--- lib/hn_reader.rb
+++ lib/hn_reader.rb
@@ -1,5 +1,5 @@
+require_relative 'hn_reader/website'
+require_relative 'hn_reader/headline_printer'
+require_relative 'hn_reader/application'
+
 module HNReader; end
-
-require_relative './hn_reader/website'
-require_relative './hn_reader/headline_printer'
-
---
+++ lib/hn_reader/application.rb
@@ -0,0 +1,7 @@
+module HNReader
+  class Application
+    def initialize
+      print ARGV.to_s
+    end
+  end
+end
