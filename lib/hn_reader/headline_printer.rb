57cc7796375394d05468ece0c831b0e93d6b0e0f
Chris Bojemski <chris.bojemski@gmail.com>
2019-01-31 22:24:15 -0500

    Adding files to gemspec

--- lib/hn_reader/headline_printer.rb
+++ lib/hn_reader/headline_printer.rb
@@ -1,12 +1,14 @@
-class HeadlinePrinter
-  def self.numbered_headlines(headline_data)
-    headline_data.each do |key, data|
-      if key < 10 
-        puts "  #{key}: #{data[0]}"
-      elsif key > 9 && key < 100
-        puts " #{key}: #{data[0]}"
-      elsif key > 99
-        puts "#{key}: #{data[0]}"
+module HNReader
+  class HeadlinePrinter
+    def self.numbered_headlines(headline_data)
+      headline_data.each do |key, data|
+        if key < 10 
+          puts "  #{key}: #{data[0]}"
+        elsif key > 9 && key < 100
+          puts " #{key}: #{data[0]}"
+        elsif key > 99
+          puts "#{key}: #{data[0]}"
+        end
       end
     end
   end
--- lib/hn_reader/website.rb
+++ lib/hn_reader/website.rb
@@ -1,51 +1,53 @@
 require 'nokogiri'
 require 'rest-client'
 
-class Website 
-  attr_reader :html
+module HNReader
+  class Website 
+    attr_reader :html
 
-  @@site_ids = {
-    1 => 'https://news.ycombinator.com',
-    2 => 'https://belong.io',
-    3 => 'https://pinboard.in/popular'
-  }
+    @@site_ids = {
+      1 => 'https://news.ycombinator.com',
+      2 => 'https://belong.io',
+      3 => 'https://pinboard.in/popular'
+    }
 
-  @@link_selectors = {
-    1 => 'a.storylink',
-    2 => 'div.item > a',
-    3 => 'a.bookmark_title'
-  }
+    @@link_selectors = {
+      1 => 'a.storylink',
+      2 => 'div.item > a',
+      3 => 'a.bookmark_title'
+    }
 
-  def initialize(site_id: 1)
-    @site_id = site_id
-  end 
+    def initialize(site_id: 1)
+      @site_id = site_id
+    end 
 
-  def site_url
-    @@site_ids[@site_id]
-  end
-
-  def link_selector
-    @@link_selectors[@site_id]
-  end
-
-  def get_link_tags
-    @link_tags = @html.css(self.link_selector)
-    if @site_id == 2
-      @link_tags.pop
+    def site_url
+      @@site_ids[@site_id]
     end
-    return @link_tags
-  end
 
-  def fetch
-    response = RestClient.get(self.site_url)
-    @html = Nokogiri::HTML(response)
-  end
-
-  def get_formatted_data
-    formatted_data = {}
-    @link_tags.each_with_index do |link_tag, i|
-      formatted_data[i + 1] = [link_tag.content, link_tag['href']]
+    def link_selector
+      @@link_selectors[@site_id]
     end
-    @formatted_data = formatted_data
+
+    def get_link_tags
+      @link_tags = @html.css(self.link_selector)
+      if @site_id == 2
+        @link_tags.pop
+      end
+      return @link_tags
+    end
+
+    def fetch
+      response = RestClient.get(self.site_url)
+      @html = Nokogiri::HTML(response)
+    end
+
+    def get_formatted_data
+      formatted_data = {}
+      @link_tags.each_with_index do |link_tag, i|
+        formatted_data[i + 1] = [link_tag.content, link_tag['href']]
+      end
+      @formatted_data = formatted_data
+    end
   end
 end
