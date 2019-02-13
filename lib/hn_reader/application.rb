33e07cf666acba6b4a22f85adf070e47d13be1cf
Chris Bojemski <chris.bojemski@gmail.com>
2019-02-12 22:42:45 -0500

    Fetching from websites once again

--- lib/hn_reader/application.rb
+++ lib/hn_reader/application.rb
@@ -1,3 +1,5 @@
+require_relative './website'
+
 module HNReader
   class Application
     attr_reader :opts, :opts_with, :opts_without
@@ -10,27 +12,53 @@ module HNReader
         :with => {},
         :without => []
       }
+
       self.parse_options
-      puts "What the heck is going on"
+      self.fetch_from_website
+    end
+
+    def fetch_from_website
+      website = nil
+      if @opts[:without].include? '--belong'
+        website = HNReader::Website.new site_id: 2
+      elsif @opts[:without].include? '--pinboard'
+        website = HNReader::Website.new site_id: 3
+      else
+        website = HNReader::Website.new site_id: 1
+      end
+
+      website.fetch
+      website.get_link_tags
+      data = website.get_formatted_data
+      puts data
     end
 
     def parse_options
       previous = nil
 
-      ARGV.to_s.split(' ').each do |opt|
+      ARGV.each_with_index do |opt, index|
+        if @opts_without.include? opt 
+          @opts[:without] << opt
+        end 
+
         if @opts_with.include? opt
           previous = opt
         end
 
         if previous
-          #print @opts
           @opts[:with][previous] = opt
           previous = nil
         end
-
       end
 
-      #print @opts
+      print @opts
     end
   end
 end
+
+=begin
+Generally we should avoid having to process flags with options. 
+All flags should default to not having them, and instead we will 
+see if the last opt is an integer. If it is, we'll only print
+out that particular link combined with the other options.
+=end
